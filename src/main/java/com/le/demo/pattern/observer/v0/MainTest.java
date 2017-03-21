505c2d86c9d79885dda825d70eb6b1f1d6ef3401
chenlele <chenlele@baidu.com>
2017-03-21 19:16:24 +0800

    添加observer

--- src/main/java/com/le/demo/pattern/observer/v0/MainTest.java
+++ src/main/java/com/le/demo/pattern/observer/v0/MainTest.java
@@ -1,4 +1,4 @@
-package com.le.demo.pattern.observer;
+package com.le.demo.pattern.observer.v0;
 
 /**
  * Created by LE on 2017/3/19.
---
+++ src/main/java/com/le/demo/pattern/observer/v0/Subject.java
@@ -0,0 +1,8 @@
+package com.le.demo.pattern.observer.v0;
+
+/**
+ * Created by LE on 2017/3/21.
+ * 发布者接口
+ */
+public interface Subject {
+}
